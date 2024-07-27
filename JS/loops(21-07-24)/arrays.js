bf83aa6be63320c2ad9ae6b7200e727f8bb7d685
Zeeshan Ali <98347114+zeeshanali90233@users.noreply.github.com>
2024-07-26 23:54:46 -0700

    26/7

---
+++ JS/loops(21-07-24)/arrays.js
@@ -0,0 +1,56 @@
+// Arrays
+let myNames = [1, 2, 50, "Ali", "Zeeshan", "12", "dsd"];
+
+// Index Always Starts from zero
+console.log(myNames[5]);
+console.log(myNames[0]);
+console.log(myNames[1]);
+console.log(myNames[2]);
+
+console.log(myNames[2]);
+
+// Number of Elements
+let lengthOfMyArray = myNames.length;
+
+console.log(lengthOfMyArray);
+
+myNames.push("Asad");
+myNames.push("Farhad");
+myNames.push("Farhan");
+myNames.push("Farhan");
+myNames.push(1);
+myNames.push(2);
+myNames.push(true);
+myNames.push(false);
+myNames.push(2.3);
+
+console.log(myNames);
+
+lengthOfMyArray = myNames.length;
+
+console.log(lengthOfMyArray);
+
+let removed = myNames.pop();
+let removed1 = myNames.pop();
+myNames.pop();
+
+console.log(myNames);
+console.log(removed);
+
+function checkEligibility(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function add(a, b) {
+  let result = a + b;
+  return result;
+}
+
+let isEligible = checkEligibility(17);
+
+let result = add(2, 5);
+console.log(result);
---
+++ JS/loops(21-07-24)/index.js
@@ -0,0 +1,86 @@
+// For Checking the Connectivity
+// alert("I am Linked");
+
+// for
+// let myName = prompt("Enter your name");
+// let numOfIter = prompt("How many times should, we print it?");
+// // Type Casting
+// numOfIter = Number(numOfIter); //5
+
+// // 0<=5 T
+// // 1<=5 T
+// // 2<=5 T
+// // 3<=5 T
+// // 4<=5 T
+// // 5<5 F
+for (let i = 0; i < numOfIter; i = i + 1) {
+  console.log(myName);
+}
+
+// While Loop
+// while (myName !== "Ali") {
+//   myName = prompt("Enter your name");
+//   console.log(myName);
+// }
+
+let isBlocked = false;
+// if (myName === "Ali" && myName !== "Zee") {
+//   isBlocked = true;
+// }
+
+if (isBlocked) {
+} else {
+}
+
+let choice = prompt("Enter 1 for add, 2 for multiply , 3 for sub");
+let result;
+// Value Based Cases
+switch (choice) {
+  case "1":
+    result = 10 + 10;
+    break;
+  case "2":
+    result = 10 * 10;
+    break;
+  case "3":
+    result = 10 - 10;
+    break;
+  default:
+    console.log("Not a Correct option ...");
+    break;
+}
+
+console.log(result);
+
+// Functions
+
+// Function Definition
+function checkAgeEligibility(age, name) {
+  if (age >= 18) {
+    console.log(name + " your are Eligible");
+  } else {
+    console.log(name + " your are not Eligible");
+  }
+}
+
+let myName = prompt("Enter your name");
+let age = prompt("Enter your age");
+age = Number(age);
+
+// Single & Multiple Parameters
+// Function Call
+checkAgeEligibility(age, myName);
+
+function sayHello() {
+  console.log("Hi!");
+}
+
+if (age == 18) {
+  sayHello();
+}
+
+let name1 = "Ali";
+let name2 = "Zee";
+let name3 = "ZeeAl";
+
+
