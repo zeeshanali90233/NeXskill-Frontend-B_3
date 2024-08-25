9d416432b956e4f303538672ae795e1ee3a81073
Zeeshan Ali <98347114+zeeshanali90233@users.noreply.github.com>
2024-08-24 23:42:03 -0700

    16 last

---
+++ JS/2D_Array/index.js
@@ -0,0 +1,42 @@
+const myNames = ["Ali", "Zee", "Farhad"];
+// 1 Directional Array  , 1D Array
+
+console.log(myNames[2]);
+
+// 2D
+// 2 Dimensional Array
+const vector_2d = [
+  [34, 5, 66],
+  [4, 5, 6, 7],
+  [6, 7, 8, 5],
+];
+
+console.log(vector_2d[1][2]);
+console.log(vector_2d[2][0]);
+
+
+// 3D
+// 3 Dimensional Array
+
+const vector_3d = [
+  [
+    ["adss", "ddd", "ds"],
+    ["adss", "ddd", "ds"],
+    ["adss", "ddd", "ds"],
+  ],
+  [
+    ["adss", "ddd", "ds"],
+    ["adss", "ddd", "ds"],
+    ["adss", "ddd", "ds"],
+  ],
+  [
+    ["adss", "ddd", "ds"],
+    ["adss", "ddd", "ds"],
+    ["adss", "ddd", "ds"],
+  ],
+];
+
+console.log(vector_3d[0][2][0]);
+console.log(vector_3d[1][1][1]);
+console.log(vector_3d[2][2][2]);
+
